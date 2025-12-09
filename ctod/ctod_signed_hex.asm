c12860ee229a3bbdf3fd9044c02f5876677db39a
ldfd <ldfd@outlook.it>
2025-12-09 21:38:18 +0800

    2 8 16进制

---
+++ ctod/ctod_signed_hex.asm
@@ -0,0 +1,123 @@
+.model small
+DATA segment
+    buffer db 100
+    db ?
+    db 100 dup(0)
+DATA ends
+
+.stack 400h
+
+CODE segment
+    assume cs:CODE, ds:DATA
+START:
+    mov ax, DATA
+    mov ds, ax
+
+    mov ah, 0Ah
+    mov dx, offset buffer
+    int 21h
+
+    lea si, buffer
+    mov cl, [si + 1]     ; 获取实际输入的字符数
+    xor ch, ch
+    add si, 2            ; SI 指向实际字符串
+    push cx              ; 压入长度
+    push si              ; 压入字符串指针
+    call ctod
+
+    int 3                ; 调试断点：此时查看 ax 寄存器的值
+    mov ah, 4Ch
+    int 21h
+    
+; 将16进制字符串转换为有符号整数函数
+; 输入: str_ptr 指向字符串的指针
+;       str_len 字符串的长度
+; 输出: ax 中存放转换后的有符号整数（16 位）
+ctod proc STDCALL str_ptr:WORD, str_len:WORD
+    push bx
+    push cx
+    push dx
+    push si
+    push di
+    mov si, str_ptr      ; SI -> 字符串开始
+    mov cx, str_len      ; CX = 字符串长度
+    xor ax, ax           ; AX = 0, accumulator
+    jcxz end_convert     ; zero length -> return 0
+
+    xor di, di           ; DI will be sign flag: 0 = positive, 1 = negative
+    mov al, [si]
+    cmp al, '-'
+    je handle_negative
+    cmp al, '+'
+    je skip_sign
+    jmp convert_loop
+    
+handle_negative:
+    mov di, 1
+    inc si
+    dec cx
+    jz end_convert
+    jmp convert_loop
+    
+skip_sign:
+    inc si
+    dec cx
+    jz end_convert
+    
+convert_loop:
+    jcxz end_convert     ; if no more characters, done
+    mov bl, [si]
+    cmp bl, '0'
+    jb end_convert
+    cmp bl, '9'
+    jbe digit_decimal
+    cmp bl, 'A'
+    jb check_lower
+    cmp bl, 'F'
+    jbe digit_upper
+    jmp check_lower
+
+digit_upper:
+    sub bl, 'A'
+    add bl, 10
+    jmp accumulate
+
+check_lower:
+    cmp bl, 'a'
+    jb end_convert
+    cmp bl, 'f'
+    ja end_convert
+    sub bl, 'a'
+    add bl, 10
+    jmp accumulate
+
+digit_decimal:
+    sub bl, '0'
+
+accumulate:
+    xor bh, bh
+    push dx
+    mov dx, 16
+    imul dx              ; ax = ax * 16 (signed multiply)
+    pop dx
+    add ax, bx
+    
+    inc si
+    loop convert_loop
+
+end_convert:
+    cmp di, 0
+    jz done_convert
+    neg ax
+
+done_convert:
+    pop di
+    pop si
+    pop dx
+    pop cx
+    pop bx
+    ret
+ctod endp
+
+CODE ends
+end START
---
+++ dtoc/dtoc_signed_oct.asm
@@ -0,0 +1,105 @@
+.model small
+
+DATA segment
+    buffer db 100 dup(0)  ; 用于存储转换后的字符串
+DATA ends
+
+.stack 400h
+
+CODE segment
+    assume cs:CODE, ds:DATA
+START:
+    mov ax, DATA
+    mov ds, ax
+
+    mov ax, -1234        ; 示例输入数据
+    lea si, buffer       ; si指向字符串缓冲区
+    call dtoc            ; 调用dtoc函数
+
+    mov ax, offset buffer ; 将字符串地址加载到dx以便查看结果
+    push ax
+    call show_str
+
+    mov ax, 4C00H
+    int 21H
+
+    
+; 名称:dtoc
+; 功能:将 word型数据转变为表示8进制数的字符串,字符串以0为结尾符。
+; 参数:(ax)=word 型数据
+; ds:si指向字符串的首地址
+dtoc proc
+    push ax
+    push bx
+    push cx
+    push dx
+    push bp
+    
+    ; 记录符号并对负数取绝对值，兼容-32768
+    cwd                     ; 符号扩展到dx
+    mov bp, dx              ; 保存符号
+    cmp bp, 0
+    jge dtoc_positive
+    mov byte ptr [si], '-'
+    inc si
+    neg ax                  ; 对dx:ax取绝对值
+    adc dx, 0
+    neg dx
+dtoc_positive:
+
+    mov bx, 8
+    xor cx, cx          ; cx用来计数位数
+
+next_digit:
+    xor dx, dx
+    div bx              ; ax = ax / 8, dx = ax % 8
+    push dx             ; 保存余数
+    inc cx              ; 位数加1
+    cmp ax, 0
+    jne next_digit
+    
+    ; 此时cx中保存了8进制数的位数
+    ; 栈中保存了各位数字(逆序)
+
+pop_digits:
+    pop dx              ; 弹出一位数字
+    add dl, 30h         ; 转为ASCII码
+    mov [si], dl        ; 保存到字符串
+    inc si
+    loop pop_digits     ; 循环处理下一位
+
+    mov byte ptr [si], 0   ; 仍保留0作为逻辑结束符
+
+    pop bp
+    pop dx
+    pop cx
+    pop bx
+    pop ax
+    ret
+dtoc endp
+
+show_str proc STDCALL str_ptr:WORD
+    push ax
+    push dx
+    ; 找到字符串的0结尾，替换成'$'
+    mov si, str_ptr
+find_end:
+    mov al, [si]
+    cmp al, 0
+    je replace_end
+    inc si
+    jmp find_end
+replace_end:
+    mov byte ptr [si], '$'
+
+    mov dx, str_ptr
+    mov ah, 09H
+    int 21H
+
+    pop dx
+    pop ax
+    ret
+show_str endp
+
+CODE ends
+END START
